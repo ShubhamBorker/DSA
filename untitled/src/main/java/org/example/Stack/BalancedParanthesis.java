36a5913465489c210396055c5f58054d8bcbd29e
ShubhamKumarBorker <shubhamborker363@gmail.com>
2026-01-10 18:39:53 +0530

    linked list

---
+++ untitled/src/main/java/org/example/Stack/BalancedParanthesis.java
@@ -0,0 +1,27 @@
+package org.example.Stack;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+    public static boolean isBalanced(String expr){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch: expr.toCharArray()){
+            if (ch =='{' || ch =='(' || ch =='['){
+                stack.push(ch);
+            } else if (ch == '}' || ch == ')' || ch ==']') {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == '}' && top !='{')|| (ch==')' && top!='(') || (ch==']' && top!='[')){
+                    return  false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[]args){
+        System.out.println(isBalanced("{[()]}"));
+        System.out.println(isBalanced("{[()]"));
+    }
+}
