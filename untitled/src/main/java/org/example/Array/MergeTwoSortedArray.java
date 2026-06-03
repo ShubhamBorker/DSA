29c1844023c74dc0ba298966ff0e900001a7e4c3
ShubhamKumarBorker <shubhamborker363@gmail.com>
2026-06-04 01:22:25 +0530

    Array

--- untitled/src/main/java/org/example/Array/MergeTwoSortedArray.java
+++ untitled/src/main/java/org/example/Array/MergeTwoSortedArray.java
@@ -35,10 +35,34 @@ public class MergeTwoSortedArray {
         return merged;
     }
 
+    public static void mergeInPlace(int[] nums1, int m, int[] nums2, int n){
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+
+        while(i>=0 &&j>=0){
+            if (nums1[i]>=nums2[j]){
+                nums1[k--]= nums1[i--];
+            } else {
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while (j>=0){
+            nums1[k--]=nums2[j--];
+        }
+
+    }
+
     public static void main(String[] args) {
         int[] arr1 = {1,3,5};
         int[] arr2 = {2,4,6};
+        int[] nums1 = {1, 3, 5, 0, 0, 0};
+        int[] nums2 = {2, 4, 6};
+
         System.out.println(Arrays.toString(mergeBrute(arr1, arr2))); // [1,2,3,4,5,6]
         System.out.println(Arrays.toString(mergeOpt(arr1,arr2)));
+
+        mergeInPlace(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
     }
 }
